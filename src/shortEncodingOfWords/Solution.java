1f312b13fea8f4d066870cc00b959fe02da22164
Anmol-Nagpal <nagpaanm@sheridancollege.ca>
2021-11-19 21:49:48 -0500

    solved shortEncodingOfWords problem (leetcode)

--- src/shortEncodingOfWords/Solution.java
+++ src/shortEncodingOfWords/Solution.java
@@ -24,21 +24,63 @@ words[2] = "bell", the substring of s starting from indices[2] = 5 to the next '
 
 public class Solution {
 	public int minimumLengthEncoding(String[] words) {
-        if(words.length == 0) {
+		if(words.length == 0) {
         	return 1;
         }
         else if(words.length == 1) {
         	return words[0].length() + 1;
         }
         // else
-        String s = words[0] + "#";
-        for(int i = 1; i < words.length; i++) {
-        	int index = (words[i - 1].indexOf(words[i]));
-        	//System.out.println(index + words[i].length() + 1 + " " + words[i - 1].length());
-        	if(index < 0 || (index + words[i].length() != words[i - 1].length())) {
-        		s += words[i] + "#";
+        
+		//this is a better solution.
+		// 1) turn into set to remove any duplicates
+		// 2) iterate over set, for each word in set:
+		// 3) for each substring in word from length 1 to word.length()
+		// 4) remove the substring from the set
+		// 5) iterate over the set and count the total. Add 1 (+1) each time 
+		// to account for the '#' that must be added.
+        /*
+        Set<String> s = new HashSet<>(Arrays.asList(words));
+        for (String w : words){
+            for (int i = 1; i < w.length(); ++i)
+                s.remove(w.substring(i));
+        }
+        int res = 0;
+        for (String w : s){
+            res += w.length() + 1;
+        }
+        return res;
+        */
+        
+        // O(n^2)
+        int count = 0;
+        for(int i = 0 ; i < words.length; i++) {
+        	boolean found = findSmallestStringContained(words, words[i], i);
+        	if(found == true) {
+        		words[i] = "";
+        	}
+        	else {
+            	count += words[i].length() + 1;
         	}
         }
-        return s.length();
+        return count;
+        
     }
+	
+	public boolean findSmallestStringContained(String[] words, String str, int i) {
+		for(int j = 0 ; j < words.length; j++) {
+			if(j != i) {
+                if(words[i] == words[j]){
+                    words[j] = "";
+                }
+				int index = words[j].lastIndexOf(str);
+				if(index != -1) {
+					if(str.length() + index == words[j].length()) {
+						return true;
+					}
+				}
+			}
+		}
+		return false;
+	}
 }
