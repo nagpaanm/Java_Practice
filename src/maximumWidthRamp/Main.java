a285c113a79b105a279273ca60005be47020845c
Anmol-Nagpal <nagpaanm@sheridancollege.ca>
2021-11-22 23:11:11 -0500

    trying to improve maximumWidthRamp (leetcode)

---
+++ src/maximumWidthRamp/Main.java
@@ -0,0 +1,11 @@
+package maximumWidthRamp;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Solution sol = new Solution();
+		int[] nums = {9,8,1,0,1,9,4,0,4,1};
+		System.out.println(sol.maxWidthRamp(nums));
+	}
+
+}
--- src/maximumWidthRamp/Solution.java
+++ src/maximumWidthRamp/Solution.java
@@ -1,5 +1,9 @@
 package maximumWidthRamp;
 
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
 /*
  * A ramp in an integer array nums is a pair (i, j) for which i < j and 
  * nums[i] <= nums[j]. The width of such a ramp is j - i.
@@ -10,23 +14,57 @@ If there is no ramp in nums, return 0.
 
 public class Solution {
 	public int maxWidthRamp(int[] nums) {
+		Queue<int[]> queue = new LinkedList<int[]>();
+		Stack<int[]> stack = new Stack<int[]>();
+		int length = nums.length;
+		int maxRamp = 0;
+		for(int i = 0; i < length - 2; i++) {
+			int[] combo = {nums[i], i};
+			stack.add(combo);
+			queue.add(combo);
+		}
+		//add last element
+		int[] combo = {nums[length - 2], length - 2};
+		stack.add(combo);
+		int[] combo11 = {nums[length - 1], length - 1};
+		stack.add(combo11);
+		while(!stack.isEmpty()) {
+			combo = stack.pop();
+			System.out.println("stack: " + combo[0] + " index: " + combo[1]);
+			for(int[] combo2: queue) {
+				System.out.println("queue: " + combo2[0] + " index: " + combo2[1]);
+				if(combo2[0] <= combo[0] && combo[1] - combo2[1] > maxRamp) {
+					maxRamp = combo[1] - combo2[1];
+					break;
+				}
+			}
+			
+			if(stack.size() < maxRamp) {
+				return maxRamp;
+			}
+		}
+		
+		return maxRamp;
+		/*
         int length = nums.length;
         int maxRamp = 0;
         int j = length - 1;
         while(0 < j){
-            for(int i = 0; i < j; i++){
+            for(int i = j - maxRamp - 1; i >= 0; i--){
                 if(nums[i] <= nums[j]){
                     if(j - i > maxRamp){
                         maxRamp = j - i;
                     }
                 }
             }
+            
             j--;
-            if(j - 0 < maxRamp){
+            if(j < maxRamp){
                 return maxRamp;
             }
         }
     
         return maxRamp;
+        */
     }
 }
