3cba9221b31561dbda38f9b4051b95666f39f7b7
Anmol-Nagpal <nagpaanm@sheridancollege.ca>
2021-11-28 12:14:33 -0500

    houseRobber3 (work in progress)

--- src/houseRober3/Solution.java
+++ src/houseRober3/Solution.java
@@ -1,6 +1,10 @@
 package houseRober3;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Solution {
+	/*
 	public int rob(TreeNode root) {
         if(root == null){
            return 0;
@@ -13,5 +17,31 @@ public class Solution {
             val += rob(root.right.left) + rob(root.right.right);
         }
         return Math.max(val + root.val, rob(root.left) + rob(root.right));
+    }
+    */
+	// dynammic programming approach. O(n) runtime.
+	
+	
+	 public int rob(TreeNode root) {
+	        return robSub(root, new HashMap<TreeNode, Integer>());
+	    }
+	    
+    public int robSub(TreeNode root, Map<TreeNode, Integer> map) {
+        if(root == null){
+           return 0;
+        }
+        if(map.containsKey(root)){
+            return map.get(root);
+        }
+        int val = 0;
+        if(root.left != null){
+            val += robSub(root.left.left, map) + robSub(root.left.right, map);
+        }
+        if(root.right != null){
+            val += robSub(root.right.left, map) + robSub(root.right.right, map);
+        }
+        val = Math.max(val + root.val, robSub(root.left, map) + robSub(root.right, map));
+        map.put(root, val);
+        return val;
     }
 }
