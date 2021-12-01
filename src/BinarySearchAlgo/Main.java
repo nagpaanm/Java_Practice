0bc9c90233404c4089892a9115a334e2056bb340
Anmol-Nagpal <nagpaanm@sheridancollege.ca>
2021-11-30 23:42:54 -0500

    improved examRoom code (leetcode)

--- src/BinarySearchAlgo/Main.java
+++ src/BinarySearchAlgo/Main.java
@@ -4,123 +4,72 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+//O(n) runtime
+class ExamRoom {
+ List<Integer> arr = new ArrayList<Integer>();
+ int length;
+ 
+ public ExamRoom(int n) {
+     length = n;
+ }
+ 
+ public int seat() {
+     int index = length;
+     int maxWidth = 0;
+     int max = 0;
+     // case 1
+     if(this.arr.size() == 0){
+         arr.add(0);
+         return 0;
+     }
+     // case 2
+     else if(arr.size() == 1){
+         int num = arr.get(0);
+         if(num < length / 2){
+             index = length - 1;
+         }else{
+             index = 0;
+         }
+     // case 3
+     }else{
+         for(int i = 0; i < arr.size() - 1; i++) {
+     	if(arr.get(i + 1) - arr.get(i) > max) {
+     		if(((arr.get(i + 1) + arr.get(i)) / 2) - arr.get(i) > maxWidth) {
+     			index = ((arr.get(i + 1) + arr.get(i)) / 2);
+     			maxWidth = ((arr.get(i + 1) + arr.get(i)) / 2) - arr.get(i);
+     			max = arr.get(i + 1) - arr.get(i);
+     		}
+     	}
+         }
+         // edge case 1: beginning of list
+         if(this.arr.get(0) >= maxWidth && !this.arr.contains(0)){
+             index = 0;
+         }
+     
+         // edge case 2: end of list
+         if(index == length){
+             index--;
+         }
+     }
+     //System.out.println(arr);
 
-public class Main {
-	
-	/*
-	 * Time Complexity of Binary Search: O(Logn). Much faster than linear 
-	 * search, which is O(n)
-	 * 
-	 * I.e. If a sorted array has 10,000,000 (10 million) elements, then
-	 * any element within the array can be located using Binary Search by 
-	 * checking roughly 24 elements - which would be SUBSTANTIALLY less than
-	 * the required number of elements had linear search been used.
-	 */
-
-	public static void main(String[] args) {
-		//List<Integer> arr = Arrays.asList(-2,3,4,7,8,9,11,13);
-		final int lowerbound = 1;
-		final int upperbound = 100000;
-		final int valueToFind = (int)Math.floor(Math.random()*(upperbound - lowerbound + 1) + lowerbound);
-		List<Integer> arr = new ArrayList<Integer>(); //polymorphism
-		for(int i = 0; i < 200000; i++) {
-			int randomNumber = (int)Math.floor(Math.random()*(upperbound - lowerbound + 1) + lowerbound);
-			if (!arr.contains(randomNumber)) {
-				arr.add(randomNumber);
-			}
-		}
-		
-		//sort the array using built-in Java module
-		long startTime = System.nanoTime();
-		Collections.sort(arr);
-		//quicksort(arr, 0, arr.size() - 1);
-		long endTime = System.nanoTime();
-		double duration = (endTime - startTime);  //nanoseconds
-		System.out.println("Quicksort: " + duration + " nanoseconds");
-		
-		// binary search - O(logn) runtime
-		startTime = System.nanoTime();
-		int index = binarySearch(arr, valueToFind);
-		endTime = System.nanoTime();
-		duration = (endTime - startTime);  //nanoseconds
-		System.out.println("Binary Search: index = " + index + ", " + duration + " nanoseconds");
-		
-		// linear search - O(n) runtime
-		startTime = System.nanoTime();
-		index = arr.indexOf(valueToFind);
-		endTime = System.nanoTime();
-		duration = (endTime - startTime);  //nanoseconds
-		System.out.println("Linear Search: index = " + index + ", " + duration + " nanoseconds");
-	}
-	
-	public static int binarySearch(List<Integer> arr, int target) {
-		/*
-		 * Return the index of target if it exists. Return -1 otherwise.
-		 * 
-		 * Assumption: all items in arr are already sorted in ascending order.
-		 */
-		
-		int left = 0;
-		int right = arr.size() - 1;
-		
-		//if left is greater than right, target was not found
-		while (left <= right) {
-			int mid = (left + right) / 2;
-			if (arr.get(mid) == target) {
-				// target is found
-				return mid;
-			}else if (arr.get(mid) < target) {
-				left = mid + 1;
-			}else {
-				right = mid - 1;
-			}
-		}
-		// target not in arr
-		return -1;
-		
-	}
-	
-	public static void quicksort(List<Integer> arr, int l, int r) {
-		/*
-		 * Quick sort algorithm.
-		 * 
-		 * Runtime Complexity:
-		 * - worst-case: Items are already sorted, or lots of duplicates. O(n^2)
-		 * - best-case: Selected pivots equally (or almost equally) seperate
-		 * 				the list in equal halves each and every time. 
-		 * 				O(nlogn)
-		 * - average-case: not many duplicates, and ordering of array is random.
-		 * 				O(nlogn)
-		 */
-		if (l >= r) {
-			return;
-		}
-		int p = partition(arr, l, r);
-		quicksort(arr, l, p - 1);
-		quicksort(arr, p + 1, r);
-	}
-	
-	public static int partition(List<Integer> arr, int l, int r) {
-		/*
-		 * Partition method for quicksort.
-		 */
-		int pivot = arr.get(r);
-		int i = l - 1;
-		for(int j = l; j < r; j++) {
-			if (arr.get(j) < pivot) {
-				i++;
-				// swap arr[i] and arr[j]
-				int temp = arr.get(j);
-				arr.set(j, arr.get(i));
-				arr.set(i, temp);
-			}	
-		}
-		// swap arr[i + 1] and arr[r]
-		int temp = arr.get(r);
-		arr.set(r, arr.get(i + 1));
-		arr.set(i + 1, temp);
-		return i + 1;
-	}
-	
-
+     addElement(this.arr, index);
+     return index;
+ }
+ 
+ public void leave(int p) {
+     int index = this.arr.indexOf(p);
+     this.arr.remove(index);
+ }
+ 
+ // add element in order
+ public void addElement(List<Integer> arr, int element){
+     for(int i = 0; i < arr.size(); i ++){
+         if(element < arr.get(i)){
+             arr.add(i, element);
+             return;
+         }
+     }
+     arr.add(arr.size(), element);
+ }
 }
