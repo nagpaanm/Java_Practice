f50d2f162a4bd3910c479dd9aa202a7b392fd3bd
Anmol-Nagpal <nagpaanm@sheridancollege.ca>
2021-12-02 23:07:29 -0500

    jumpGame2 (work in progress)

--- src/jumpGame2/Solution.java
+++ src/jumpGame2/Solution.java
@@ -10,12 +10,12 @@ public class Solution {
     
     public int jumpCount(int[] nums, int index, int num){
         //System.out.println(index + num + " " + index);
-        if(index + num >= nums.length - 1){
+        if(nums[index] + index >= nums.length - 1){
             return 1;
         }
         if(num >= 1){
             return Math.min(1 + jumpCount(nums, index + nums[index], nums[index]), 1 + jumpCount(nums, index + nums[index] - 1, nums[index] - 1));
         }
-        return 1000;
+        return 100000;
     }
 }
