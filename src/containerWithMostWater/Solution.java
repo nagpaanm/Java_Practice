1175fb349108d4536b8c54d5bc52017b1382b3bf
Anmol-Nagpal <nagpaanm@sheridancollege.ca>
2021-12-05 16:44:13 -0500

    solved containerWithMostWater problem (leetcode)

--- src/containerWithMostWater/Solution.java
+++ src/containerWithMostWater/Solution.java
@@ -3,9 +3,11 @@ package containerWithMostWater;
 public class Solution {
 	public int maxArea(int[] height) {
         int maxVolume = 0;
-        //O(n^2)
         for(int i = 0; i < height.length - 1; i++){
             for(int j = i + 1; j < height.length; j++){
+                if(maxVolume >= height[i] * height.length - 1){
+                    break;
+                }
                 int volume = Math.min(height[i], height[j]) * (j - i);
                 maxVolume = Math.max(volume, maxVolume);
             }
