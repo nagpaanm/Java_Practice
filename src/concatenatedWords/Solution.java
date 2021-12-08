54c2920eff553660425ddda6091eef19997d8463
Anmol-Nagpal <nagpaanm@sheridancollege.ca>
2021-12-07 23:03:41 -0500

    concatenatedWords (work in progress)

--- src/concatenatedWords/Solution.java
+++ src/concatenatedWords/Solution.java
@@ -10,20 +10,38 @@ public class Solution {
         int left = 0;
         int right = words.length - 1;
         String s = words[right];
+        List<String> temp = new ArrayList<String>();
         while(right > 0){
             if(left == right){
-                if(s.length() <= 0){
+                if(recurse(temp, words[right], 0) == true){
                     arr.add(words[right]);
                 }
                 left = 0;
                 right--;
                 s = words[right];
             }
+            if(words[right].contains(words[left]) && words[right].length() > words[left].length()){
+                temp.add(words[left]);
+            }
             //s = s.replace(words[left], "");
-            System.out.println(s);
+            //System.out.println(s);
             left++;
         }
         
         return arr;
+    }
+    
+    public boolean recurse(List<String> temp, String word, int index){
+        if(word.length() == 0){
+            return true;
+        }
+        boolean exp = false;
+        for(int i = index; i < temp.size(); i++){
+            String alt = word.replace(temp.get(i), "");
+            System.out.println(alt);
+            exp = recurse(temp, alt, index + 1);
+        }
+        
+        return exp;
     }
 }
