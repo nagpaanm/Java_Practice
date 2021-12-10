3c6134cf99ecf37fd93384630d0b35e55345b3cf
Anmol-Nagpal <nagpaanm@sheridancollege.ca>
2021-12-09 21:55:15 -0500

    concatenatedWords (work in progress)

--- src/PermutationWords/Solution.java
+++ src/PermutationWords/Solution.java
@@ -7,8 +7,9 @@ public class Solution {
 
 	public void permute(List<String> arr, List<String> temp, List<ArrayList<String>> main) {
 		if(temp.size() == arr.size()) {
-			main.add((ArrayList<String>) temp);
-			System.out.println(main);
+			ArrayList<String> al = new ArrayList<String>(temp);
+			main.add(al);
+			//System.out.println(main);
 			return;
 		}
 		for(int i = 0; i < arr.size(); i++) {
--- src/concatenatedWords/Solution.java
+++ src/concatenatedWords/Solution.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Solution {
+	List<ArrayList<String>> main = new ArrayList<ArrayList<String>>();
 	public List<String> findAllConcatenatedWordsInADict(String[] words) {
         List<String> arr = new ArrayList<String>();
         
@@ -12,19 +13,27 @@ public class Solution {
         String s = words[right];
         List<String> temp = new ArrayList<String>();
         while(right > 0){
-            if(left == right){
-                if(recurse(temp, words[right], 0) == true){
-                    arr.add(words[right]);
-                }
+            if(left == words.length  - 1){ //right
+            	permute(temp, new ArrayList<String>());
+            	for(ArrayList<String> ary: this.main) {
+                    s = words[right];
+            		for(String str: ary) {
+            			s = s.replace(str, "");
+            		}
+            		if(s.length() == 0) {
+            			arr.add(words[right]);
+            			break;
+            		}
+            	}
                 left = 0;
                 right--;
                 s = words[right];
+                main = new ArrayList<ArrayList<String>>();
+                temp = new ArrayList<String>();
             }
             if(words[right].contains(words[left]) && words[right].length() > words[left].length()){
                 temp.add(words[left]);
             }
-            //s = s.replace(words[left], "");
-            //System.out.println(s);
             left++;
         }
         
@@ -33,6 +42,8 @@ public class Solution {
     
 	public void permute(List<String> arr, List<String> temp) {
 		if(temp.size() == arr.size()) {
+			ArrayList<String> al = new ArrayList<String>(temp);
+            main.add(al);
 			return;
 		}
 		for(int i = 0; i < arr.size(); i++) {
