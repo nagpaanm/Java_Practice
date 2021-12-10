4b47b50a38cc99f59279391a10b24451b680f77a
Anmol-Nagpal <nagpaanm@sheridancollege.ca>
2021-12-09 20:56:39 -0500

    permute of list (completed)

--- src/PermutationWords/Main.java
+++ src/PermutationWords/Main.java
@@ -7,12 +7,17 @@ public class Main {
 
 	public static void main(String[] args) {
 		List<String> arr = new ArrayList<String>();
+		List<ArrayList<String>> main = new ArrayList<ArrayList<String>>();
 		arr.add("dog");
 		arr.add("rat");
 		arr.add("cat");
 		arr.add("cats");
 		Solution sol = new Solution();
-		sol.permute(arr, new ArrayList<String>());
+		sol.permute(arr, new ArrayList<String>(), main);
+		System.out.println(main);
+		for(List<String> a : main) {
+			System.out.println(a.size());
+		}
 	}
 
 }
--- src/PermutationWords/Solution.java
+++ src/PermutationWords/Solution.java
@@ -1,21 +1,20 @@
 package PermutationWords;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Solution {
 
-	public void permute(List<String> arr, List<List<String>> temp) {
+	public void permute(List<String> arr, List<String> temp, List<ArrayList<String>> main) {
 		if(temp.size() == arr.size()) {
-			for(int i = 0; i < temp.size(); i++){
-				System.out.print(temp.get(i));
-			}
-			System.out.print("\n");
+			main.add((ArrayList<String>) temp);
+			System.out.println(main);
 			return;
 		}
 		for(int i = 0; i < arr.size(); i++) {
 			if(!temp.contains(arr.get(i))) {
 				temp.add(arr.get(i));
-				permute(arr, temp);
+				permute(arr, temp, main);
 				temp.remove(temp.size() - 1);
 			}
 		}
