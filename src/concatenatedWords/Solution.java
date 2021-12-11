ac1606b5a5a908defc5752b1f15ed4e237f24731
Anmol-Nagpal <nagpaanm@sheridancollege.ca>
2021-12-10 22:05:04 -0500

    concatenatedWords (work in progress)

--- src/concatenatedWords/Solution.java
+++ src/concatenatedWords/Solution.java
@@ -4,31 +4,22 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Solution {
-	List<ArrayList<String>> main = new ArrayList<ArrayList<String>>();
+	boolean found = false;
 	public List<String> findAllConcatenatedWordsInADict(String[] words) {
         List<String> arr = new ArrayList<String>();
         
         int left = 0;
         int right = words.length - 1;
-        String s = words[right];
         List<String> temp = new ArrayList<String>();
         while(right > 0){
-            if(left == words.length  - 1){ //right
-            	permute(temp, new ArrayList<String>());
-            	for(ArrayList<String> ary: this.main) {
-                    s = words[right];
-            		for(String str: ary) {
-            			s = s.replace(str, "");
-            		}
-            		if(s.length() == 0) {
-            			arr.add(words[right]);
-            			break;
-            		}
-            	}
+            if(left == words.length - 1){ //right
+            	permute(temp, new ArrayList<String>(), words[right]);
+                if(this.found == true){
+                    arr.add(words[right]);
+                    this.found = false;
+                }
                 left = 0;
                 right--;
-                s = words[right];
-                main = new ArrayList<ArrayList<String>>();
                 temp = new ArrayList<String>();
             }
             if(words[right].contains(words[left]) && words[right].length() > words[left].length()){
@@ -40,16 +31,23 @@ public class Solution {
         return arr;
     }
     
-	public void permute(List<String> arr, List<String> temp) {
+	public void permute(List<String> arr, List<String> temp, String s) {
 		if(temp.size() == arr.size()) {
-			ArrayList<String> al = new ArrayList<String>(temp);
-            main.add(al);
+            for(int i = 0; i < temp.size(); i++){
+                s = s.replace(temp.get(i), "");
+                if(i >= 1 && s.length() == 0){
+                    this.found = true;
+                }
+            }
 			return;
 		}
 		for(int i = 0; i < arr.size(); i++) {
 			if(!temp.contains(arr.get(i))) {
 				temp.add(arr.get(i));
-				permute(arr, temp);
+				permute(arr, temp, s);
+                if(this.found == true){
+                    return;
+                }
 				temp.remove(temp.size() - 1);
 			}
 		}
