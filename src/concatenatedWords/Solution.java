ee9a9b41553e0d43d4d2e06376fe4c3d362412aa
Anmol-Nagpal <nagpaanm@sheridancollege.ca>
2021-12-12 21:01:57 -0500

    concatenatedWords problem (tried re-arranging array in least to greatest string length)

--- src/concatenatedWords/Solution.java
+++ src/concatenatedWords/Solution.java
@@ -6,24 +6,49 @@ import java.util.List;
 public class Solution {
 	 boolean found = false;
 		public List<String> findAllConcatenatedWordsInADict(String[] words) {
+	        List<String> arranged = new ArrayList<String>();
+	        for(String word: words){
+	            if(arranged.size() == 0){
+	                arranged.add(word);
+	            }
+	            else{
+	                if(word.length() <= arranged.get(0).length()){
+	                    arranged.add(0, word);
+	                }
+	                else{
+	                    int left = 0;
+	                    int right = arranged.size() - 1;
+	                    while(left <= right){
+	                        int mid = (left + right) / 2;
+	                        if(arranged.get(mid).length() <= word.length()){
+	                            left++;
+	                        }else{
+	                            right--;
+	                        }
+	                    }
+	                    arranged.add(left, word);
+	                }
+	            }
+	        }
+	        //System.out.println(arranged);
 	        List<String> arr = new ArrayList<String>();
 	        
 	        int left = 0;
-	        int right = words.length - 1;
+	        int right = arranged.size() - 1;
 	        List<String> temp = new ArrayList<String>();
 	        while(right > 0){
-	            if(left == words.length - 1){ //right
-	            	permute(temp, new ArrayList<String>(), words[right]);
+	            if(left == right){ //right
+	            	permute(temp, new ArrayList<String>(), arranged.get(right));
 	                if(this.found == true){
-	                    arr.add(words[right]);
+	                    arr.add(arranged.get(right));
 	                    this.found = false;
 	                }
 	                left = 0;
 	                right--;
 	                temp = new ArrayList<String>();
 	            }
-	            if(words[right].contains(words[left]) && words[right].length() > words[left].length()){
-	                temp.add(words[left]);
+	            if(arranged.get(right).contains(arranged.get(left)) && arranged.get(right).length() > arranged.get(left).length()){
+	                temp.add(arranged.get(left));
 	            }
 	            left++;
 	        }
@@ -33,7 +58,7 @@ public class Solution {
 	    
 		public void permute(List<String> arr, List<String> temp, String s) {
 			if(temp.size() == arr.size()) {
-	            System.out.println(temp);
+	            //System.out.println(temp);
 	            for(int i = 0; i < temp.size(); i++){
 	                s = s.replace(temp.get(i), "");
 	                if(i >= 1 && s.length() == 0){
