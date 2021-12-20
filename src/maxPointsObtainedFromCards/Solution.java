cc45666b225a7eaa8ce6b3213445a9e2717be0ae
Anmol-Nagpal <nagpaanm@sheridancollege.ca>
2021-12-19 19:52:53 -0500

    solved maxPointsObtainedFromCards problem (leetcode)

--- src/maxPointsObtainedFromCards/Solution.java
+++ src/maxPointsObtainedFromCards/Solution.java
@@ -1,26 +1,38 @@
 package maxPointsObtainedFromCards;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Solution {
 	public int maxScore(int[] cardPoints, int k) {
-        int cardsPicked = 0;
-        int total = 0;
-        int left = 0;
-        int right = cardPoints.length - 1;
-        while(left <= right && cardsPicked < k){
-            int i = cardPoints[left];
-            int j = cardPoints[right];
-            if(i > j){
-                left++;
-                total += i;
-            }
-            else{
-                right--;
-                total += j;
-            }
-            //System.out.println(total);
-            cardsPicked++;
+        int t = 0;
+        for(int p: cardPoints){
+            t += p;
+        }
+        if(k >= cardPoints.length){
+            return t;
+        }
+        if(k == 1){
+            return Math.max(cardPoints[0], cardPoints[cardPoints.length - 1]);
         }
         
+        List<Integer> arr = new ArrayList<Integer>();
+        int total = 0;
+        int arrTotal = 0;
+        for(int i = 0; i <= cardPoints.length; i++){
+            if(arr.size() >= cardPoints.length - k){
+                total = Math.max(total, t - arrTotal);
+                int r = arr.get(0);
+                arr.remove(0);
+                arrTotal -= r;
+            }
+            if(i == cardPoints.length){
+                break;
+            }
+            arr.add(cardPoints[i]);
+            arrTotal += cardPoints[i];
+        }
+
         return total;
     }
 }
