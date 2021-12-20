b849679803946f9d42fc2dd89d81240ed86a2b02
Anmol-Nagpal <nagpaanm@sheridancollege.ca>
2021-12-19 19:59:02 -0500

    improved maxPointsFromCards solution (leetcode)

--- src/maxPointsObtainedFromCards/Solution.java
+++ src/maxPointsObtainedFromCards/Solution.java
@@ -5,23 +5,25 @@ import java.util.List;
 
 public class Solution {
 	public int maxScore(int[] cardPoints, int k) {
-        int t = 0;
-        for(int p: cardPoints){
-            t += p;
-        }
         if(k >= cardPoints.length){
+            int t = 0;
+            for(int p: cardPoints){
+                t += p;
+            }
             return t;
         }
         if(k == 1){
             return Math.max(cardPoints[0], cardPoints[cardPoints.length - 1]);
         }
+        // else
         
         List<Integer> arr = new ArrayList<Integer>();
+        int min = Integer.MAX_VALUE;
         int total = 0;
         int arrTotal = 0;
         for(int i = 0; i <= cardPoints.length; i++){
             if(arr.size() >= cardPoints.length - k){
-                total = Math.max(total, t - arrTotal);
+                min = Math.min(min, arrTotal);
                 int r = arr.get(0);
                 arr.remove(0);
                 arrTotal -= r;
@@ -31,8 +33,9 @@ public class Solution {
             }
             arr.add(cardPoints[i]);
             arrTotal += cardPoints[i];
+            total += cardPoints[i];
         }
 
-        return total;
+        return total - min;
     }
 }
