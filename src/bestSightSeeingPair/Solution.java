8bcd7adefba1fb7abaae73c069228b6ba0a8200f
Anmol-Nagpal <nagpaanm@sheridancollege.ca>
2021-12-20 20:18:41 -0500

    added docs to bestSightSeeingPair solution

--- src/bestSightSeeingPair/Solution.java
+++ src/bestSightSeeingPair/Solution.java
@@ -1,5 +1,17 @@
 package bestSightSeeingPair;
 
+/*
+ * You are given an integer array values where values[i] represents the value 
+ * of the ith sightseeing spot. Two sightseeing spots i and j have a distance 
+ * j - i between them.
+
+The score of a pair (i < j) of sightseeing spots is 
+values[i] + values[j] + i - j: the sum of the values of the sightseeing spots, 
+minus the distance between them.
+
+Return the maximum score of a pair of sightseeing spots.
+ */
+
 public class Solution {
 	/*
     public int maxScoreSightseeingPair(int[] values) {
