f6681f3656491da5c8b3316284e25facf8fe769b
Anmol-Nagpal <nagpaanm@sheridancollege.ca>
2022-01-03 14:06:36 -0500

    solved waysToMakeAFairArray problem (leetcode)

--- src/waysToMakeAFairArray/Solution.java
+++ src/waysToMakeAFairArray/Solution.java
@@ -17,33 +17,47 @@ Return the number of indices that you could choose such that after the removal,
 nums is fair.
  */
 
-
+// Solution -- O(n) runtime
 public class Solution {
 	public int waysToMakeFair(int[] nums) {
         int left = 0;
         int ways = 0;
+        int totalOddSoFar = 0;
+        int totalEvenSoFar = 0;  
+        int totalEven = 0;
         int totalOdd = 0;
-        int totalEven = 0;  
-        while(left != nums.length){
-            int even = totalEven;
-            int odd = totalOdd;
-            for(int i = left + 1; i < nums.length; i++){
-                if(i % 2 == 0){
-                    odd += nums[i];
-                }
-                else{
-                    even += nums[i];
-                }
+        int even = 0;
+        int odd = 0;
+        //calculate total of even and odd
+        for(int i = 0 ; i < nums.length; i++){
+            if(i % 2 == 0){
+                totalEven += nums[i];
+            }else{
+                totalOdd += nums[i];
             }
-            
+        }
+        while(left != nums.length){
+            if(left % 2 == 0){
+                // even = total even so far + total odd - total odd so far
+                even = totalEvenSoFar + totalOdd - totalOddSoFar;
+                // odd  = total odd so far + total even - total even so far - nums[left]
+                odd = totalOddSoFar + totalEven - totalEvenSoFar - nums[left];
+            }else{
+                // even = total even so far + total odd - total odd so far + nums[left]
+                even = totalEvenSoFar + totalOdd - totalOddSoFar - nums[left];
+                // odd = total even - total even so far + total odd so far
+                odd = totalEven - totalEvenSoFar + totalOddSoFar;
+            }
             if(even == odd){
                 ways++;
             }
             if(left % 2 == 0){
-                totalEven += nums[left];
+                // total even so far
+                totalEvenSoFar += nums[left];
             }
             else{
-                totalOdd += nums[left];
+                // total odd so far
+                totalOddSoFar += nums[left];
             }
             left++;
         }
